0cd2f39c3c829e2a7824f3eb5f1cc111403b1d6b
coder19 <89542999+PilakaPreddy@users.noreply.github.com>
2022-05-28 21:14:22 +0530

    Add files via upload

---
+++ celsius_to_fahrenheit.java
@@ -0,0 +1,15 @@
+package final_practiceexamcodes;
+
+public class celsius_to_fahrenheit {
+
+	public static void main(String[] args) {
+		double celsius = 1;
+		double fahreinheit;
+		
+		fahreinheit=(celsius *1.8)+32;
+		System.out.println("value of temp in fahreinheit is " +fahreinheit);
+		
+
+	}
+
+}
